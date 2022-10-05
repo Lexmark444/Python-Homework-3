1056adacba6df901cc49f0070e0521e4d2a39c90
Lexmark444 <104227345+Lexmark444@users.noreply.github.com>
2022-10-05 17:49:34 -0400

    Add files via upload

---
+++ module.py
@@ -0,0 +1,13 @@
+def printName(name):
+    return f"Hello Mr/Ms {name}...we've been waiting for you!"
+
+def squareFootage(length, width):
+    area = length * width
+    print('Area = ' + str(area) + 'sqft')
+
+
+def circleCircum(radius):
+    from math import pi
+    circumference = 2 * pi * radius
+    print('Circumference = ' + str(circumference) + "inches")
+  
